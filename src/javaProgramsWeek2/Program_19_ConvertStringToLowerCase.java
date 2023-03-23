157e18cbea7c89e428e7668a4e8fd5037e13a223
shweta158dave <shweta158dave@gmail.com>
2023-03-23 23:26:48 +0000

    My homework week2-writing

---
+++ src/javaProgramsWeek2/Program_19_ConvertStringToLowerCase.java
@@ -0,0 +1,31 @@
+package javaProgramsWeek2;
+
+/**
+ * Write a Java program to convert a given string into lowercase.
+ * Sample Input: THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+ * Output: the quick brown fox jumps over the lazy dog.
+ */
+
+import java.util.Scanner;
+
+public class Program_19_ConvertStringToLowerCase {
+
+
+    public static void main(String[] args) {
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the uppercase sentence: ");
+        String uppercase = scanner.nextLine();
+        Program_19_ConvertStringToLowerCase t = new Program_19_ConvertStringToLowerCase();
+        t.convertStringToLowerCase(uppercase);
+        //Closing the scanner object
+        scanner.close();
+
+    }
+
+    // Convert string the to lower case
+    public void convertStringToLowerCase(String str) {
+        System.out.println("The Lowercase of the string is = " + str.toLowerCase());
+    }
+
+}
---
+++ src/javaProgramsWeek2/Program_4.java
@@ -0,0 +1,52 @@
+package javaProgramsWeek2;
+/**
+ *
+ * Write a Java programme using the following steps.
+ * 4.1 Declare two instance and two static variables.
+ * 4.2 Declare one instance method.
+ * 4.3 Declare one static method.
+ * 4.4 Call all four instance and static variables into both instance and static methods inside the print statement.
+ * 4.5 Declare the Main method.
+ * 4.6 Call both instance and static methods into the Main method and run the programme.
+ */
+public class Program_4 {
+
+    //4.1 Declare two instance and two static variables.
+    //instance variable
+    String Name = "Core";
+    String Surname = "JAva";
+    //static variable
+    static boolean a = true;
+    static boolean b = false;
+
+    //4.2 Declare one instance method.
+    //4.4 Call all four instance and static variables into both instance and static methods inside the print statement.
+    public void instanceMethod() {
+        System.out.println(Name);
+        System.out.println(Surname);
+        System.out.println(Program_4.a);
+        System.out.println(Program_4.b);
+    }
+
+    //4.3 Declare one static method.
+    //4.4 Call all four instance and static variables into both instance and static methods inside the print statement.
+    public static void staticMethod() {
+        Program_4 programme_4 = new Program_4();
+        System.out.println(programme_4.Name);
+        System.out.println(programme_4.Surname);
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    //4.5 Declare the Main method.
+    //4.6 Call both instance and static methods into the Main method and run the programme.
+    public static void main(String[] args) {
+        Program_4 programme_4 = new Program_4();
+        programme_4.instanceMethod();
+        staticMethod();
+    }
+}
+
+
+
+
